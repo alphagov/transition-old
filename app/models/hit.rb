b610f89a956e6645b0207c2dfc0f4e1c54f60f5d
Murray Steele <muz@h-lame.com>
2013-05-09 09:51:56 +0100

    Lazily evaluate calculations in most recent hit data object

--- app/models/hit.rb
+++ app/models/hit.rb
@@ -36,6 +36,10 @@ class Hit < ActiveRecord::Base
     scoped.group(:http_status).sum(:count)
   end
 
+  def self.total_hits
+    scoped.sum(:count)
+  end
+
   def set_path_hash
     self.path_hash = Digest::SHA1.hexdigest(self.path) if self.path_changed?
   end
--- app/models/most_recent_hit_data.rb
+++ app/models/most_recent_hit_data.rb
@@ -1,12 +1,20 @@
 class MostRecentHitData
-  attr_reader :most_recent_hit_on_date, :hits, :biggest_hit_count,
-              :hit_counts_by_status, :total_hits
+  attr_reader :most_recent_hit_on_date, :hits
 
   def initialize(hit_scope, hit_on_date)
     @most_recent_hit_on_date = hit_on_date
     @hits = hit_scope.without_zero_status_hits.most_recent_hits(@most_recent_hit_on_date).in_count_order
-    @biggest_hit_count = @hits.most_hits
-    @hit_counts_by_status = @hits.counts_by_status
-    @total_hits = @hits.sum(:count)
+  end
+
+  def biggest_hit_count
+    @biggest_hit_count ||= @hits.most_hits
+  end
+
+  def hit_counts_by_status
+    @hit_counts_by_status ||= @hits.counts_by_status
+  end
+
+  def total_hits
+    @total_hits ||= @hits.total_hits
   end
 end
