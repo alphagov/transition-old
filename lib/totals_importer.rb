72382a570503027f3fd0be99c84ad901a0ab5c8a
Murray Steele <muz@h-lame.com>
2013-05-29 17:22:44 +0100

    Make the changes to totals_importer that we just made to hits_importer

--- lib/totals_importer.rb
+++ lib/totals_importer.rb
@@ -2,28 +2,41 @@ require 'csv'
 class TotalsImporter
   def initialize(data_file)
     @data_file = data_file
-    @hosts = {}
   end
 
   def import!
-    consume_data_file do |total_row|
+    consume_data do |total_row|
       hostname = total_row[:host]
-      next if hostname =~ /^aka/
-      host = @hosts[hostname] ||= Host.find_by_host(hostname)
+      next if hostname =~ /^aka/      
+      host = hosts[hostname]
       if host.nil?
         $stderr.puts "ERROR: Host missing #{hostname}, can't import #{total_row.inspect}"
       else
-        total = find_or_create_total_for_host_from_row(host, total_row)
-        total.count = total_row[:count]
-        if !total.save
-          $stderr.puts "ERROR: Can't import #{total_row.inspect}, #{total.errors.full_messages}"
+        result = create_or_update_total_for_host_from_row(host, total_row)
+        unless result[:ok]
+          $stderr.puts "ERROR: Can't import #{total_row.inspect}, #{result[:total].errors.full_messages}"
         end
       end
     end
   end
 
+  def fetch_total_scope(host, hit_row)
+    Total.where(host_id: host.id, http_status: total_row[:status], total_on: total_row[:date])
+  end
+
+  def create_or_update_total_for_host_from_row(host, total_row)
+    update_hit(fetch_total_scope(host, total_row).build, total_row[:count])
+  rescue ActiveRecord::RecordNotUnique
+    update_hit(fetch_total_scope(host, total_row).first, total_row[:count])
+  end
+
+  def update_total(total, count)
+    total.count = count
+    {ok: total.save, hit: total}
+  end
+
   def find_or_create_total_for_host_from_row(host, total_row)
-    total_scope = Total.where(host_id: host.id, http_status: total_row[:status], total_on: total_row[:date])
+    total_scope = fetch_total_scope(host, total_row)
     if total_scope.first.present?
       total_scope.first
     else
@@ -31,12 +44,24 @@ class TotalsImporter
     end
   end
 
-  def consume_data_file
-    CSV.new(File.open(@data_file), { headers: true,
-                                     converters: [:numeric, :date],
-                                     header_converters: :symbol,
-                                     col_sep: "\t" }).each do |total_row|
-      yield total_row
+  def hosts
+    @hosts ||= Host.all.inject({}) { |m, h| m.merge(h.host => h) }
+  end
+
+  def consume_data(&block)
+    Dir[@data_file].each do |data_file|
+      $stdout.puts "Processing: #{data_file}"
+      consume_data_file(data_file, &block)
+      $stdout.puts "\n"
+    end
+  end
+
+  def consume_data_file(data_file)
+    CSV.new(File.open(data_file), { headers: true,
+                                    converters: [:numeric, :date],
+                                    header_converters: :symbol,
+                                    col_sep: "\t" }).each do |hit_row|
+      yield hit_row
     end
   end
 end
