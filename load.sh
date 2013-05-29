6926d81626cf321aeef96087b93145fd9cb2c2c9
psd <psd@whatfettle.com>
2013-05-29 17:37:10 +0100

    moved clone to jenkins task

--- load.sh
+++ load.sh
@@ -1,26 +1,10 @@
 #!/bin/sh
 
+# depends upon:
+# git clone --depth 1 git@github.com:alphagov/redirector.git data/redirector
+# git clone --depth 1 git@github.com:alphagov/transition-stats data/transition-stats
+
 set -x
-rm -rf data
-mkdir data
-
-git clone --depth 1 git@github.com:alphagov/redirector.git data/redirector
-git clone --depth 1 git@github.com:alphagov/transition-stats data/transition-stats
-
 rake db:drop db:create db:migrate db:seed
-
-set +x
-
-for file in data/transition-stats/totals/*.tsv
-do
-    set -x
-    bundle exec rake import_totals[$file]
-    set +x
-done
-
-for file in data/transition-stats/hits/*.tsv
-do
-    set -x
-    bundle exec rake import_hits[$file]
-    set +x
-done
+bundle exec rake 'import_totals[data/transition-stats/totals/*.tsv]'
+bundle exec rake 'import_hits[data/transition-stats/hits/*.tsv]'
