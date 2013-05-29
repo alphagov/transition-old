776efea4bc957a0b0ceba3bba1b6d69ffbbc5d25
psd <psd@whatfettle.com>
2013-05-29 17:50:14 +0100

    bundle exec rake

--- load.sh
+++ load.sh
@@ -5,6 +5,6 @@
 # git clone --depth 1 git@github.com:alphagov/transition-stats data/transition-stats
 
 set -x
-rake db:drop db:create db:migrate db:seed
+bundle exec rake db:drop db:create db:migrate db:seed
 bundle exec rake 'import_totals[data/transition-stats/totals/*.tsv]'
 bundle exec rake 'import_hits[data/transition-stats/hits/*.tsv]'
