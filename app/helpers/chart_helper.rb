55eacf24afd26b4217ed9f0a7cc7595c1e5a13a0
Murray Steele <muz@h-lame.com>
2013-06-03 13:05:34 +0100

    Fill in numbers properly on the little org dashboard widget

--- app/helpers/chart_helper.rb
+++ app/helpers/chart_helper.rb
@@ -1,7 +1,7 @@
 module ChartHelper
 
   def summation_line_for_label(label, data, max_count, chart_height, title_prefix)
-    count = (data[label] || []).inject(0) {|sum, total| sum + total.count }
+    count = count_data_for_label(label, data)
     height =
       if max_count > 0
         (count * chart_height) / max_count
@@ -10,5 +10,10 @@ module ChartHelper
       end
     content_tag(:li, count, class: label, style: "height: #{height}px", title: "#{title_prefix} #{label} #{count}")
   end
+
+  def count_data_for_label(label, data)
+    (data[label] || []).inject(0) { |sum, x| sum + x.count }
+  end
+
 
 end
--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -1,5 +1,24 @@
 class Organisation < ActiveRecord::Base
   has_many :sites
+  has_many :hosts, through: :sites
+  has_many :mappings, through: :sites
+  has_many :totals, through: :hosts
+
+  def sites_count
+    sites.count
+  end
+
+  def mappings_count
+    mappings.count
+  end
+
+  def hosts_count
+    hosts.count
+  end
+
+  def aggregated_totals
+    self.totals.aggregated
+  end
 
   def to_param
     abbr
