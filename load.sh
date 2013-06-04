1e4f976771cf2a5e0e934d47510298c72306cc83
Murray Steele <muz@h-lame.com>
2013-06-04 11:27:15 +0100

    Add some more import tasks to load.sh
    1. dns lookup for hosts
    2. setting the manages_own_redirects flag for orgs

--- load.sh
+++ load.sh
@@ -3,8 +3,12 @@
 # depends upon:
 # git clone --depth 1 git@github.com:alphagov/redirector.git data/redirector
 # git clone --depth 1 git@github.com:alphagov/transition-stats data/transition-stats
+# being able to dig for the cname of each host
 
 set -x
 bundle exec rake db:drop db:create db:migrate db:seed
 bundle exec rake 'import_totals[data/transition-stats/totals/*.tsv]'
 bundle exec rake 'import_hits[data/transition-stats/hits/*.tsv]'
+bundle exec rake 'import_mappings[data/redirector/mappings/*.tsv]'
+bundle exec rake 'fetch_hosts_dns'
+bundle exec rake 'update_organisation_redirect_flag'
