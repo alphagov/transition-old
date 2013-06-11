5198f0d9e0000c3eb06ae4f8cec1175de5d48d95
Edd Sowden <e@e26.co.uk>
2013-06-11 12:19:35 +0100

    Add first version of pages controller
    - Updated status charts to let it have links to pages view filtered by
      week
    - Added base frontend stylesheet and IE specific versions version
    - Added hit range model and added methods to hit model to power it
    - Added the date formats from whitehall for standard GOV.UK style dates

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,15 +6,41 @@ class PagesController < ApplicationController
 
   def show
     @organisation = Organisation.find_by_abbr!(params[:id])
-    if params[:host]
-      @obj = Host.find_by_host(params[:id])
-    elsif params[:site]
-      @obj = Site.find_by_site(params[:id])
-    else
+    if @organisation.sites.count == 1
+      @site = @organisation.sites.first
+      @obj = @site
+    elsif params[:site] && params[:site] != ''
+      @site = Site.find_by_id(params[:site])
+      @obj = @site
+    end
+    if @site && @site.hosts.count == 1
+      @host = @site.hosts.first
+      @obj = @host
+    elsif params[:host] && params[:host] != ''
+      @host = Host.find_by_id(params[:host])
+      @obj = @host
+    end
+    if @obj.nil?
       @obj = @organisation
     end
-    @total_data = WeeklyTotalData.new(@obj.weekly_totals, Date.new(2012,10,17)..Date.today)
-    @most_recent_hit_data = MostRecentHitData.new(@obj.hits, @obj.hits.most_recent_hit_on_date)
+    @total_data = WeeklyTotalData.new(@obj.weekly_totals, Date.new(2012,10,17)..Date.today, true)
+    if params[:start]
+      @start_date = DateTime.strptime(params[:start], '%Y-W%U')
+      @end_date = @start_date + 7.days
+      @most_recent_hit_data = HitRangeData.new(@obj.hits.includes(:host), @start_date, @end_date)
+    else
+      if params[:date]
+        begin
+          @start_date = DateTime.strptime(params[:date], '%Y-%m-%d')
+        rescue
+          raise ActionController::RoutingError.new('Not Found')
+        end
+      end
+      unless @start_date
+        @start_date = @obj.hits.most_recent_hit_on_date
+      end
+      @most_recent_hit_data = MostRecentHitData.new(@obj.hits.includes(:host), @start_date)
+    end
 
     respond_to do |format|
       format.html # show.html.erb
