d9f5c45507250b0a61ae978f994ccf2e3ad34a14
Jolyon Pawlyn <jolyon.pawlyn@unboxedconsulting.com>
2013-07-05 15:31:06 +0100

    Let's add more detail to the url page feature test

--- spec/features/url_spec.rb
+++ spec/features/url_spec.rb
@@ -5,13 +5,13 @@ feature "View url for an organisation" do
     login_as_stub_user
     @organisation = create :organisation, title: 'DFID'
     @site = create :site, organisation: @organisation
-    create :url, url: 'http://www.naturalengland.org.uk/', site: @site
-    @url1 = create :url, url: 'http://www.naturalengland.org.uk/about_us/default.aspx', site: @site
-    create :url, url: 'http://www.naturalengland.org.uk/contact_us', site: @site
+    @url1 = create :url, url: 'http://www.naturalengland.org.uk/', site: @site
+    @url2 = create :url, url: 'http://www.naturalengland.org.uk/about_us/default.aspx', site: @site
+    @url3 = create :url, url: 'http://www.naturalengland.org.uk/contact_us', site: @site
   end
 
   scenario "Url page with adjacent urls and iframe" do
-    visit organisation_url_path(@organisation, @url1)
+    visit organisation_url_path(@organisation, @url2)
 
     page.should have_link('DFID', href: organisation_path(@organisation))
     should_have_table 'table thead', [
@@ -20,6 +20,9 @@ feature "View url for an organisation" do
       ['http://www.naturalengland.org.uk/'],
       ['http://www.naturalengland.org.uk/about_us/default.aspx'],
       ['http://www.naturalengland.org.uk/contact_us']]
-    page.should have_xpath("//iframe[@src='#{@url1.url}']")
+    page.should have_link(@url1.url, href: organisation_url_path(@organisation, @url1))
+    page.should_not have_link(@url2.url)
+    page.should have_link(@url3.url, href: organisation_url_path(@organisation, @url3))
+    page.should have_xpath("//iframe[@src='#{@url2.url}']")
   end
 end
