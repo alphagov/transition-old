d3e34d32330290f4b053a5f90ac7ed2ad63aaaf7
Jolyon Pawlyn <jolyon.pawlyn@unboxedconsulting.com>
2013-07-08 12:29:54 +0100

    Provide mapping for manual URL

--- app/models/url.rb
+++ app/models/url.rb
@@ -2,6 +2,7 @@ class Url < ActiveRecord::Base
   include Workflow
 
   belongs_to :site
+  delegate :new_url, to: :mapping, allow_nil: true
 
   # validations
   validates :url, uniqueness: {case_sensitive: false}
@@ -19,5 +20,40 @@ class Url < ActiveRecord::Base
 
     state :archived         # Terminal, 410
     state :redirected       # Terminal, 301
+  end
+
+  def manual(new_url = nil)
+    set_mapping_url(new_url) if new_url
+  end
+
+  # return mapping if there is one
+  def mapping
+    return nil if host.nil?
+    @mapping ||= site.mappings.find_by_path(request_uri)
+  end
+
+  # if existing mapping found then update path else create maaping
+  def set_mapping_url(new_url)
+    raise "No site host found for #{url}" if host.nil?
+    if mapping
+      mapping.update_attributes!(new_url: new_url)
+    else
+      map = site.mappings.build(new_url: new_url, path: request_uri, http_status: '301')
+      map.save!
+    end
+  end
+
+  private
+
+  def host
+    @host ||= site.hosts.find_by_host(uri.host)
+  end
+
+  def request_uri
+    @request_uri ||= uri.path + (uri.query.present? ? '?' + uri.query : '')
+  end
+
+  def uri
+    @uri ||= URI.parse(url)
   end
 end
