7fec686ce088759962334816a5ccd9d348fa4346
Russell Garner <rgarner@zephyros-systems.co.uk>
2013-07-08 10:35:54 +0100

    Tidy url_helper spec

--- spec/helpers/urls_helper_spec.rb
+++ spec/helpers/urls_helper_spec.rb
@@ -1,8 +1,12 @@
 require "spec_helper"
 
-describe '#destiny_buttons' do
-  it 'creates a button with the right class, name, and title' do
-    markup = helper.destiny_button(:manual)
-    markup.should == '<button class="manual" name="destiny" type="submit" value="manual">Manual</button>'
+describe '#destiny_button' do
+  context 'when creating a button for the state "manual"' do
+    subject { helper.destiny_button(:manual) }
+
+    it { should include('name="destiny"') }
+    it { should include('class="manual"') }
+    it { should include('type="submit"') }
+    it { should include('value="manual"') }
   end
 end
