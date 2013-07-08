aac963be5b52c1761a03b786e45ef16b448d2459
Russell Garner <rgarner@zephyros-systems.co.uk>
2013-07-08 17:17:12 +0100

    Make destiny_button helper indicate selected

--- app/helpers/urls_helper.rb
+++ app/helpers/urls_helper.rb
@@ -1,5 +1,10 @@
 module UrlsHelper
-  def destiny_button(destiny)
-    button_tag(destiny.to_s.titleize, name: 'destiny', class: destiny, value: destiny)
+  def destiny_button(url, destiny)
+    button_tag(
+        destiny.to_s.titleize,
+        name: 'destiny',
+        class: "#{url.workflow_state == destiny ? 'selected ' : ''}#{destiny}",
+        value: destiny
+    )
   end
 end
--- spec/helpers/urls_helper_spec.rb
+++ spec/helpers/urls_helper_spec.rb
@@ -1,12 +1,25 @@
-require "spec_helper"
+require 'spec_helper'
 
 describe '#destiny_button' do
-  context 'when creating a button for the state "manual"' do
-    subject { helper.destiny_button(:manual) }
+  context 'the URL is :new and we''re creating a button for the state "manual"' do
+    let(:url) { FactoryGirl.build(:url) }
+
+    subject { helper.destiny_button(url, :manual) }
 
     it { should include('name="destiny"') }
     it { should include('class="manual"') }
     it { should include('type="submit"') }
     it { should include('value="manual"') }
+  end
+
+  context 'the URL has a state of :archive and we''re creating a button for the state "archive"' do
+    let(:url) { FactoryGirl.create(:url, workflow_state: :archive) }
+
+    subject { helper.destiny_button(url, :archive) }
+
+    it { should include('name="destiny"') }
+    it { should include('class="selected archive"') }
+    it { should include('type="submit"') }
+    it { should include('value="archive"') }
   end
 end
