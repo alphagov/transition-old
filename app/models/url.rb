c89829561aa5eb3860958364a1b10bf8a879e05e
Jolyon Pawlyn <jolyon.pawlyn@unboxedconsulting.com>
2013-07-08 08:46:52 +0100

    Add turbolinks

--- app/models/url.rb
+++ app/models/url.rb
@@ -5,4 +5,28 @@ class Url < ActiveRecord::Base
   validates :url, uniqueness: {case_sensitive: false} 
   validates :site, presence: true
 
+  workflow do
+    state :new do
+      event :archive, :transitions_to => :archived
+      event :manual, :transitions_to => :manual
+    end
+
+    state :manual do
+      event :redirect, :transitions_to => :redirected
+    end
+
+    state :archived         # Terminal, 410
+    state :redirected       # Terminal, 301
+  end
+
+  # return mapping if there is one
+  def mapping
+    uri = URI.parse(url)
+    host_name = uri.host
+    request_uri = uri.path
+    request_uri += '?' + uri.query if uri.query.present?
+    host = site.hosts.find_by_host(host_name)
+    mapping = site.mappings.find_by_path(request_uri) if host
+    mapping
+  end
 end
--- spec/models/url_spec.rb
+++ spec/models/url_spec.rb
@@ -12,4 +12,65 @@ describe Url do
     end
     it { should validate_presence_of(:site) }
   end
+
+  describe 'URL workflow' do
+    subject(:url) { FactoryGirl.create(:url) }
+
+    it { should be_new }
+
+    describe 'archiving URLs' do
+      before { url.archive! }
+
+      it { should be_archived }
+    end
+
+    describe 'Indicating URLs will have manual content for them' do
+      context "when we don''t have a to_url" do
+        before { url.manual! }
+
+        it { should be_manual }
+
+        it 'creates no mapping' do
+          pending 'not creating mappings yet'
+        end
+      end
+
+      context 'when we have a to_url' do
+        let(:to_url) { 'http://goes.here/' }
+
+        before { url.manual!(to_url) }
+
+        it { should be_manual }
+
+        it 'creates a mapping' do
+          pending 'not creating mappings yet'
+        end
+      end
+    end
+  end
+
+  context :mapping do
+    before :each do
+      @site = create :site
+      @host1 = create :host, host: 'www.attorney-general.gov.uk', site: @site
+      @host2 = create :host, host: 'www.ago.gov.uk', site: @site
+      @host3 = create :host, host: 'www.dfid.gov.uk'
+      @mapping1 = create :mapping, path: '/this-is-a-url?hello', site: @site
+    end
+
+    it 'should find a mapping with the url http://www.attorney-general.gov.uk/this-is-a-url?hello' do
+      url = create :url, url: 'http://www.attorney-general.gov.uk/this-is-a-url?hello', site: @site
+      url.mapping.should == @mapping1
+    end
+
+    it 'should find a mapping with the url www.ago.gov.uk/this-is-a-url?hello' do
+      url = create :url, url: 'http://www.ago.gov.uk/this-is-a-url?hello', site: @site
+      url.mapping.should == @mapping1
+    end
+
+    it 'should not find a mapping with the url www.dfid.gov.uk/this-is-a-url?hello' do
+      url = create :url, url: 'http://www.dfid.gov.uk/this-is-a-url?hello', site: @site
+      url.mapping.should be_nil
+    end
+  end
 end
