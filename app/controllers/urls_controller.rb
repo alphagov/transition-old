06c92efee38331fbb36f15e64816e8589f469b61
Jolyon Pawlyn <jolyon.pawlyn@unboxedconsulting.com>
2013-07-09 09:03:06 +0100

    If a site, organisation or url model is not found, display a 404 page as opposed to raising an exception

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,10 +1,10 @@
 class UrlsController < ApplicationController
   def index
     if params[:site_id]
-      @site = Site.find_by_site(params[:site_id])
+      @site = Site.find_by_site!(params[:site_id])
       @organisation = @site.organisation
     else
-      @organisation = Organisation.find_by_abbr(params[:organisation_id])
+      @organisation = Organisation.find_by_abbr!(params[:organisation_id])
     end
 
     @url_org = @site || @organisation
@@ -17,14 +17,14 @@ class UrlsController < ApplicationController
   end
 
   def show
-    @site = Site.find_by_site(params[:site_id])
+    @site = Site.find_by_site!(params[:site_id])
     @organisation = @site.organisation
-    @url = @organisation.urls.find_by_id(params[:id])
+    @url = @organisation.urls.find(params[:id])
   end
 
   def update
     destiny = params[:destiny].to_sym
-    url = Url.find_by_id(params[:id])
+    url = Url.find(params[:id])
     case destiny
       when :manual
         url.manual!(params[:new_url])
