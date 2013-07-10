22cbb273f82493ee765c9dae7e90fdac699fb048
Jolyon Pawlyn <jolyon.pawlyn@unboxedconsulting.com>
2013-07-10 09:32:48 +0100

    Add link to per site url list

---
+++ spec/features/organisation_spec.rb
@@ -0,0 +1,18 @@
+require 'features/features_helper'
+
+feature "View Organisation page with associated sites" do
+  before do
+    login_as_stub_user
+    @organisation = create :organisation, title: 'DFID'
+    @site1 = create :site, organisation: @organisation, site: 'DFID-site1'
+    @site2 = create :site, organisation: @organisation, site: 'DFID-site2'
+  end
+
+  scenario "Organisation page should have links to site urls" do
+    visit organisation_path(@organisation)
+
+    page.should have_content('DFID')
+    page.should have_link('View DFID-site1 urls', href: site_urls_path(@site1))
+    page.should have_link('View DFID-site2 urls', href: site_urls_path(@site2))
+  end
+end
