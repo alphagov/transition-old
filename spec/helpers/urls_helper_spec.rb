ca09af6f15795e2c287535281aab946b953a4e31
Tom Stuart <tom@codon.com>
2013-07-18 17:32:53 +0100

    Remove unused #destiny_button helper

--- spec/helpers/urls_helper_spec.rb
+++ spec/helpers/urls_helper_spec.rb
@@ -1,29 +1,5 @@
 require 'spec_helper'
 
-describe '#destiny_button' do
-  context 'the URL is :new and we''re creating a button for the state "unsure"' do
-    let(:url) { FactoryGirl.build(:url) }
-
-    subject { helper.destiny_button(url, :unsure) }
-
-    it { should include('name="destiny"') }
-    it { should include('class="unsure"') }
-    it { should include('type="submit"') }
-    it { should include('value="unsure"') }
-  end
-
-  context 'the URL has a state of :unsure and we''re creating a button for the state "unsure"' do
-    let(:url) { create(:url, workflow_state: :unsure) }
-
-    subject { helper.destiny_button(url, :unsure) }
-
-    it { should include('name="destiny"') }
-    it { should include('class="selected unsure"') }
-    it { should include('type="submit"') }
-    it { should include('value="unsure"') }
-  end
-end
-
 describe '#url_classy_link' do
   context 'the URL is :new' do
     let(:url)         { build(:url, id: 1) }
