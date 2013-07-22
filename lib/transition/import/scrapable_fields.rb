99a04e56a84ecebc987c1ef63ff127fc47dacaee
Jolyon Pawlyn <jolyon.pawlyn@unboxedconsulting.com>
2013-07-22 15:53:48 +0100

    Add scrappable fields to News content types

--- lib/transition/import/scrapable_fields.rb
+++ lib/transition/import/scrapable_fields.rb
@@ -13,6 +13,13 @@ module Transition
           type.scrapable_fields = [title, summary, body, published_date]
           type.save!
         end
+
+        ['Press release', 'News story', 'Government response'].each do |content_type|
+          ContentType.where(type: 'News article', subtype: content_type).first!.tap do |type|
+            type.scrapable_fields = [title, summary, body, published_date]
+            type.save!
+          end
+        end
       end
     end
   end
