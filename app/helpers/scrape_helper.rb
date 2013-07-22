c0e7d301450de0efbc9ae4ddb092f45256305096
Jolyon Pawlyn <jolyon.pawlyn@unboxedconsulting.com>
2013-07-22 11:27:43 +0100

    Add toolbar for rich text editor and add published data scrappable field.

--- app/helpers/scrape_helper.rb
+++ app/helpers/scrape_helper.rb
@@ -2,12 +2,15 @@ module ScrapeHelper
 
   def scrape_field_and_label(field, scrape_result)
     field_name = "scrape_result[#{field.name}]"
-    html = label_tag(field_name, field.name.capitalize)
-    html += case field.type.to_sym
-    when :string
+    html = label_tag(field_name, field.name.humanize)
+    html += if field.type.to_sym == :string
       text_field_tag(field_name, scrape_result.field_value(field.name))
-    when :text
-      rich_text_editor_toolbar + text_area_tag(field_name, scrape_result.field_value(field.name), id: 'wysihtml5-textarea')
+    elsif field.type.to_sym == :date
+      text_field_tag(field_name, scrape_result.field_value(field.name), class: 'datepicker')
+    elsif field.type.to_sym == :text and field.name == 'body'
+      rich_text_editor_toolbar + text_area_tag(field_name, scrape_result.field_value(field.name))
+    elsif field.type.to_sym == :text
+      text_area_tag(field_name, scrape_result.field_value(field.name))
     end
     html
   end
@@ -15,7 +18,16 @@ module ScrapeHelper
   def rich_text_editor_toolbar
     %Q{
       <span id="wysihtml5-toolbar" style="display: none;">
-        <a data-wysihtml5-action="change_view" href="javascript:;" unselectable="on" class="wysihtml5-action-active">html view</a>
+        <a data-wysihtml5-command="bold"></a>
+        <a data-wysihtml5-command="italic"></a>
+        <a data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h1"></a>
+        <a data-wysihtml5-command="formatBlock" data-wysihtml5-command-value="h2"></a>
+        <a data-wysihtml5-command="createLink"></a>
+        <div data-wysihtml5-dialog="createLink" style="display: none;">
+          <label>Link:<input data-wysihtml5-dialog-field="href" value="http://" class="text"></label>
+          <a data-wysihtml5-dialog-action="save">OK</a> <a data-wysihtml5-dialog-action="cancel">Cancel</a>
+        </div>
+        <a data-wysihtml5-action="change_view" href="javascript:;" unselectable="on" class="wysihtml5-action-active"></a>
       </span>
     }.html_safe
   end
