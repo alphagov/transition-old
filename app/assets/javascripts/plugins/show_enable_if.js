44f51cff786594875c47912c281d5e6e0955a1a7
Russell Garner <rgarner@zephyros-systems.co.uk>
2013-07-23 16:44:39 +0100

    Use 'disabled' rather than 'readonly' on for_scraping

--- app/assets/javascripts/plugins/show_enable_if.js
+++ app/assets/javascripts/plugins/show_enable_if.js
@@ -1,8 +1,8 @@
 jQuery.fn.showEnableIf = function(show) {
   if(show) {
-      $(this).show().find('input').removeAttr('readonly');
+      $(this).show().find('input').removeAttr('disabled');
   } else {
-      $(this).hide().find('input').attr('readonly', 'readonly');
+      $(this).hide().find('input').attr('disabled', 'disabled');
   }
 
   return $(this);
