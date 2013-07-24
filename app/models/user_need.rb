319eec665cbb76bcb7e1e284ffd2c1090b97e006
Russell Garner <rgarner@zephyros-systems.co.uk>
2013-07-24 14:20:24 +0100

    Create a needotron_id on save

--- app/models/user_need.rb
+++ app/models/user_need.rb
@@ -1,7 +1,16 @@
 class UserNeed < ActiveRecord::Base
+  ARBITRARY_NEEDOTRON_ID_GAP = 5000
+  after_create :assign_needotron_id
+
   # relationships
   belongs_to :organisation
 
   # validations
   validates :name, presence: true, uniqueness: {case_sensitive: false}
+
+  private
+
+  def assign_needotron_id
+    update_attribute(:needotron_id, id + ARBITRARY_NEEDOTRON_ID_GAP)
+  end
 end
--- spec/models/user_need_spec.rb
+++ spec/models/user_need_spec.rb
@@ -9,4 +9,9 @@ describe UserNeed do
     it { should validate_presence_of(:name) }
     it { should validate_uniqueness_of(:name).case_insensitive }
   end
+
+  it 'generates needotron-compatible ids' do
+    need = create :user_need
+    need.needotron_id.should == need.id + UserNeed::ARBITRARY_NEEDOTRON_ID_GAP
+  end
 end
