d0cf4d572678ded4b978f64e8645ba4de4351a95
Jolyon Pawlyn <jolyon.pawlyn@unboxedconsulting.com>
2013-07-25 13:10:07 +0100

    Remove optgroups from user needs dropdown

--- app/helpers/user_needs_helper.rb
+++ app/helpers/user_needs_helper.rb
@@ -1,16 +1,12 @@
 module UserNeedsHelper
 
   def user_needs_select
-    options = {}
-    user_needs_group = UserNeed.includes(:organisation).order('organisations.title', 'user_needs.name').group_by(&:organisation)
-    user_needs_group.each do |org, user_needs|
-      options[org.title] = user_needs.map do |user_need| 
-        [user_need.name, user_need.id, {
+    options = UserNeed.includes(:organisation).order('user_needs.name').map do |user_need|
+      ["#{user_need.name} (#{user_need.organisation.abbr})", user_need.id, {
         'data-as-a'      => user_need.as_a,
         'data-i-want-to' => user_need.i_want_to,
         'data-so-that'   => user_need.so_that}]
-      end
     end
-    grouped_options_for_select(options)
+    options_for_select(options)
   end
 end
--- spec/features/user_needs_spec.rb
+++ spec/features/user_needs_spec.rb
@@ -3,8 +3,8 @@ require 'features/features_helper'
 feature 'View, create, edit and delete user needs' do
   background do
     login_as_stub_user
-    organisation1 = create :organisation, title: 'DFID'
-    organisation2 = create :organisation, title: 'BIS'
+    organisation1 = create :organisation, title: 'Department for International Department',  abbr: 'DFID'
+    organisation2 = create :organisation, title: 'Department for Business, Innovation and Skills', abbr: 'BIS'
     @user_need = create :user_need, name: 'Project overview', organisation: organisation1,
      as_a: 'Agent', i_want_to: 'Improve customer service', so_that: 'Tax payers are happy'
   end
@@ -13,7 +13,7 @@ feature 'View, create, edit and delete user needs' do
     visit root_path
     click_link 'User needs'
 
-    page.should have_xpath('//optgroup[@label="DFID"]/option[text()="Project overview"]')
+    page.should have_xpath('//option[text()="Project overview (DFID)"]')
     page.should_not have_link('Edit')
 
     select 'Project overview'
@@ -22,27 +22,30 @@ feature 'View, create, edit and delete user needs' do
     page.should have_content 'Tax payers are happy'
     click_link 'Edit'
 
-    page.should have_xpath('//option[text()="DFID" and @selected="selected"]')
+    page.should have_xpath('//option[text()="Department for International Department" and @selected="selected"]')
     page.should have_field('Name', with: 'Project overview')
+    page.should have_field('As a', with: 'Agent')
+    page.should have_field('I want to', with: 'Improve customer service')
+    page.should have_field('So that', with: 'Tax payers are happy')
 
-    select 'BIS'
+    select 'Department for Business, Innovation and Skills'
     fill_in 'Name', with: 'Mission'
     click_button 'Save'
 
-    page.should have_xpath('//optgroup[@label="BIS"]/option[text()="Mission"]')
+    page.should have_xpath('//option[text()="Mission (BIS)"]')
   end
 
   scenario 'Create a new user need' do
     visit user_needs_path
 
-    select 'BIS', from: 'Organisation'
+    select 'Department for Business, Innovation and Skills', from: 'Organisation'
     fill_in('Name', with: 'Passport renewal')
     fill_in('As a', with: "Agent on her Majesty's secret service")
     fill_in('I want to', with: "Ski and run")
     fill_in('So that', with: "I stay fit")
     click_button 'Save'
 
-    page.should have_xpath('//optgroup[@label="BIS"]/option[text()="Passport renewal"]')
+    page.should have_select('user_need_id', options: ['', 'Passport renewal (BIS)', 'Project overview (DFID)'])
   end
 
   scenario 'Delete a user need' do
@@ -50,6 +53,6 @@ feature 'View, create, edit and delete user needs' do
 
     click_link 'Delete'
 
-    page.should have_no_xpath('//optgroup[@label="DFID"]/option[text()="Project overview"]')
+    page.should have_select('user_need_id', options: [''])
   end
 end
