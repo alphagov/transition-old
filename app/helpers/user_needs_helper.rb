ffc33152ba5716ddf01e15fb0bf316d94237f1cb
Jolyon Pawlyn <jolyon.pawlyn@unboxedconsulting.com>
2013-07-25 09:53:55 +0100

    Tidy up following review comments

--- app/helpers/user_needs_helper.rb
+++ app/helpers/user_needs_helper.rb
@@ -2,10 +2,9 @@ module UserNeedsHelper
 
   def user_needs_select
     options = {}
-    user_needs_group = UserNeed.joins(:organisation).order('organisations.title', 'user_needs.name').
-      group_by {|user_need| user_need.organisation}
-    user_needs_group.keys.each do |org|
-      options[org.title] = user_needs_group[org].map {|user_need| [user_need.name, user_need.id]}
+    user_needs_group = UserNeed.includes(:organisation).order('organisations.title', 'user_needs.name').group_by(&:organisation)
+    user_needs_group.each do |org, user_needs|
+      options[org.title] = user_needs.map { |user_need| [user_need.name, user_need.id] }
     end
     grouped_options_for_select(options)
   end
