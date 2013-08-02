16cf0a1b388c8131bcdd30ed08bf8cae2d936e15
Russell Garner <rgarner@zephyros-systems.co.uk>
2013-08-02 10:46:48 +0100

    Spec correction

--- spec/lib/transition/import/raw/urls_spec.rb
+++ spec/lib/transition/import/raw/urls_spec.rb
@@ -13,13 +13,15 @@ require "spec_helper"
 describe Transition::Import::Raw::Urls do
   describe '.import_file', expensive_setup: true do
     before(:all) do
+      # import_file just calls Optic14n::CanonicalizedUrls.from_urls(File.read(filename).each_line, allow_query: :all)
+      # The important thing is allow_query: :all - querystrings are being stripped
       Transition::Import::Raw::Urls.import_file(fixture_file('voa_abridged.txt'))
     end
 
+    specify { ::Raw::ImportedFile.first.urls_seen.should == 3 }
     specify { ::Raw::Url.all.should have(2).urls }
     specify { ::Raw::Url.first.url.should == 'http://www.voa.gov.uk' }
-    specify { ::Raw::Url.last.url.should == 'http://www.voa.gov.uk' }
-    specify { ::Raw::ImportedFile.first.urls_seen.should == 3 }
+    specify { ::Raw::Url.last.url.should == 'http://www.voa.gov.uk/stuff' }
 
     describe 'the failed URL' do
       subject { Raw::FailedUrl.first }
