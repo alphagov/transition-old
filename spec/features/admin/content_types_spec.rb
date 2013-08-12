17c56e0d2f8cffe2b3b21733336e84e77c2f512d
Russell Garner <rgarner@zephyros-systems.co.uk>
2013-08-12 14:06:01 +0100

    Remove intermittently failing drag/drop test
    See https://www.pivotaltracker.com/story/show/54993046

--- spec/features/admin/content_types_spec.rb
+++ spec/features/admin/content_types_spec.rb
@@ -5,32 +5,12 @@ feature 'View, create or edit content types' do
     login_as_stub_user
   end
 
-  describe :index_edit do
+  describe 'Index / edit' do
     let!(:content_type1) { create :content_type, type: 'Type 1', subtype: 'Subtype 1', scrapable: true, 
                                   user_need_required: false, mandatory_guidance: false }
     let!(:content_type2) { create :content_type, type: 'Type 2', subtype: nil, scrapable: false, 
                                   user_need_required: true, mandatory_guidance: true }
   
-    scenario 'Visit the contents page and swap the order of the two contents type', js: true do
-      visit admin_root_path
-      click_link 'Content types'
-
-      page.should have_exact_table 'table thead', [
-        ['Type / Subtype', 'Scrapable?', 'User need required?', 'Mandatory guidance?', '']]
-      page.should have_exact_table 'table tbody', [
-        ['Type 1 / Subtype 1', 'Yes',    '',    '', ''],
-        ['Type 2',                '', 'Yes', 'Yes', '']]
-
-      drop_place = find('table tbody tr:nth-child(2) td:nth-child(1) a')
-      find('table tbody tr:nth-child(1) td:nth-child(1) a').drag_to(drop_place)
-
-      # reload page to check that re-ordering has occurred
-      visit admin_content_types_path
-      page.should have_exact_table 'table tbody', [
-        ['Type 2',                '', 'Yes', 'Yes', ''],
-        ['Type 1 / Subtype 1', 'Yes',    '',    '', '']]
-    end
-
     scenario 'Edit a content type' do
       visit admin_content_types_path
       click_link 'Type 1 / Subtype 1'
