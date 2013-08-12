20cb511c6bcf63867b4e9450b9326dc6511be0a1
Russell Garner <rgarner@zephyros-systems.co.uk>
2013-08-12 11:15:04 +0100

    Separate Capybara extensions and matchers that just happen to use Capybara

--- spec/features/features_helper.rb
+++ spec/features/features_helper.rb
@@ -5,8 +5,4 @@ require 'capybara/poltergeist'
 Capybara.javascript_driver = :poltergeist
 Capybara.default_wait_time = 4
 
-RSpec.configure do |config|
-
-end
-
 Dir[File.expand_path(File.join(File.dirname(__FILE__),'support','**','*.rb'))].each {|f| require f}
