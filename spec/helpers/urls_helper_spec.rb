2e4aeb4a0f6e2dcb1fb00f0470a4a3a4f06a43c1
Russell Garner <rgarner@zephyros-systems.co.uk>
2013-08-12 16:23:23 +0100

    Fix helper description

--- spec/helpers/urls_helper_spec.rb
+++ spec/helpers/urls_helper_spec.rb
@@ -1,21 +1,23 @@
 require 'spec_helper'
 
-describe '#url_classy_link' do
-  context 'the URL is :new' do
-    let(:url)         { build(:url, id: 1) }
-    let(:current_url) { build(:url, url: 'http://elsewhere', id: 2) }
+describe UrlsHelper do
+  describe '#classy_link' do
+    context 'the URL is :new' do
+      let(:url)         { build(:url, id: 1) }
+      let(:current_url) { build(:url, url: 'http://elsewhere', id: 2) }
 
-    context "it's the current URL" do
-      subject { helper.classy_link(url, url) }
+      context "it's the current URL" do
+        subject { helper.classy_link(url, url) }
 
-      it { should eql(url.url) }
-    end
+        it { should eql(url.url) }
+      end
 
-    context "it's visible in the list, but not current" do
-      subject { helper.classy_link(url, current_url) }
+      context "it's visible in the list, but not current" do
+        subject { helper.classy_link(url, current_url) }
 
-      it { should include('<a') }
-      it { should include('class="new"') }
+        it { should include('<a') }
+        it { should include('class="new"') }
+      end
     end
   end
 end
