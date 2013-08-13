93c4bcd7d4aa3b9fccc8b2907cca3936bd558248
Russell Garner <rgarner@zephyros-systems.co.uk>
2013-08-13 09:40:06 +0100

    Add show_counts option to content type dropdown
    (this doesn't fix the issue that a helper is issuing (quite expensive) ActiveRecord calls)

--- app/helpers/urls_helper.rb
+++ app/helpers/urls_helper.rb
@@ -10,16 +10,19 @@ module UrlsHelper
     )
   end
 
-  def entry_for_content_type(content_type)
-    [
-      content_type.subtype.presence || content_type.type,
-      content_type.id,
-      {
-        'data-user_need_required' => content_type.user_need_required,
-        'data-scrapable'          => content_type.scrapable,
-        'data-mandatory_guidance' => content_type.mandatory_guidance
-      }
-    ]
+  def entry_for_content_type(content_type, options)
+    show_counts = options[:show_counts]
+    data = {
+      'data-user_need_required' => content_type.user_need_required,
+      'data-scrapable'          => content_type.scrapable,
+      'data-mandatory_guidance' => content_type.mandatory_guidance
+    }
+    data['data-url-count']      = content_type.url_count if show_counts
+
+    display_name = (content_type.subtype.presence || content_type.type)
+    display_name << " (#{content_type.url_count})" if (show_counts == true && content_type.url_count > 0)
+
+    [ display_name, content_type.id, data ]
   end
 
   def options_for_url_state_select(state)
@@ -32,13 +35,31 @@ module UrlsHelper
     options_for_select(options, for_scrape)
   end
 
-  def grouped_options_for_content_type_select(url_or_content_type_id)
-    content_type_id = url_or_content_type_id.is_a?(Url) ? url_or_content_type_id.content_type_id : url_or_content_type_id
-    ContentType.order(:position).group_by(&:type).map do |type, content_types|
+  def grouped_options_for_content_type_select(url_or_content_type_id, options = {})
+    selected_content_type_id = if url_or_content_type_id.is_a?(Url) then
+                                 url_or_content_type_id.content_type_id
+                               else
+                                 url_or_content_type_id
+                               end
+
+    types_with_url_counts = if options[:show_counts]
+                              ContentType.select('c.*, COUNT(u.id) AS url_count').
+                                from('content_types c').
+                                joins('LEFT JOIN urls u on u.content_type_id = c.id').
+                                group('c.type, c.subtype, c.scrapable, c.mandatory_guidance, c.position').
+                                order(:position)
+                            else
+                              ContentType.order(:position)
+                            end
+
+    types_with_url_counts.group_by(&:type).map do |type, content_types|
       if content_types.one? && (content_type = content_types.first).subtype.blank?
-        options_for_select([entry_for_content_type(content_type)], content_type_id)
+        options_for_select([entry_for_content_type(content_type, options)], selected_content_type_id)
       else
-        grouped_options_for_select({ type => content_types.map { |content_type| entry_for_content_type(content_type) } }, content_type_id)
+        grouped_options_for_select(
+          { type => content_types.map { |content_type| entry_for_content_type(content_type, options) } },
+          selected_content_type_id
+        )
       end
     end.join.html_safe
   end
--- spec/helpers/urls_helper_spec.rb
+++ spec/helpers/urls_helper_spec.rb
@@ -20,4 +20,75 @@ describe UrlsHelper do
       end
     end
   end
+
+  describe 'grouped_options_for_content_type_select', expensive_setup: true do
+    let(:url)          { create :url_with_content_type }
+    let(:content_type) { create :content_type }
+
+    # Yes, this helper method talks to the database. No, don't ask.
+    before do
+      # Make sure these exist
+      [content_type, url]
+    end
+
+    shared_examples 'it groups' do
+      it 'groups the Publication in an <optgroup>' do
+        rendered.should include('optgroup label="Publication"')
+      end
+    end
+
+    shared_examples 'it counts' do
+      it 'has one item with a count' do
+        rendered.should include('data-url-count="1"')
+        rendered.should include("#{url.content_type.subtype} (1)")
+      end
+
+      it 'has one item without' do
+        rendered.should include('data-url-count="0"')
+        rendered.should include(content_type.subtype)
+      end
+    end
+
+    shared_examples 'it does not count' do
+      it 'does not have data-url-count=' do
+        rendered.should_not include('data-url-count=')
+      end
+
+      it 'does not have bracketed display counts' do
+        rendered.should_not include(' (1)')
+      end
+    end
+
+    context ':show_counts is true' do
+      context 'a url is given' do
+        subject(:rendered) { helper.grouped_options_for_content_type_select(url, show_counts: true) }
+
+        it_behaves_like 'it groups'
+        it_behaves_like 'it counts'
+      end
+
+      context 'a content type id is given' do
+        subject(:rendered) { helper.grouped_options_for_content_type_select(url.content_type_id, show_counts: true) }
+
+        it_behaves_like 'it groups'
+        it_behaves_like 'it counts'
+      end
+    end
+
+    context ':show_counts is false/not given' do
+      context 'a url is given' do
+        subject(:rendered) { helper.grouped_options_for_content_type_select(url) }
+
+        it_behaves_like 'it groups'
+        it { should_not include(' (1)') }
+      end
+
+      context 'a content type id is given' do
+        subject(:rendered) { helper.grouped_options_for_content_type_select(url.content_type_id) }
+
+        it_behaves_like 'it groups'
+        it { should_not include(' (1)') }
+      end
+    end
+  end
 end
