358870f0f8f7a0cfdde47577e8b4aab2d9536a75
Jamie Cobbett <jamie.cobbett@digital.cabinet-office.gov.uk>
2013-08-19 10:00:55 +0000

    Undo accidental change

--- config/initializers/disable_editing.rb
+++ config/initializers/disable_editing.rb
@@ -3,4 +3,4 @@
 # A feature flag to turn off everything analysis-related to enable bouncer to go
 # into production while having the confidence that NOTHING CAN BE EDITED HERE
 # https://www.pivotaltracker.com/story/show/55153408
-DISABLE_EDITING = true
+DISABLE_EDITING = false
