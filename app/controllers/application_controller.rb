alphagov/transition-old

commit 8f4f86382d4b5d0ec926add4fe06deebc31d013b

Refactor read-only control
This reduces complexity and line-count, but also ensures that new controllers
get the functionality automatically (unless they don't inherit from
ApplicationController).

app/controllers/application_controller.rb
@@ -2,13 +2,18 @@ class ApplicationController < ActionController::Base
   include GDS::SSO::ControllerMethods
 
   before_filter :require_signin_permission!
+  before_filter :require_action_enabled!
 
   protect_from_forgery
 
   def error_400; error 400; end
   def error_404; error 404; end
+  def error_501(unavailable_error)
+    render status: 501, text: unavailable_error.to_s
+  end
 
   rescue_from ActiveRecord::RecordNotFound, with: :error_404
+  rescue_from Transition::FeatureUnavailableError, with: :error_501
 
   protected
 
@@ -20,5 +25,14 @@ class ApplicationController < ActionController::Base
 
   def error(status_code)
     render status: status_code, text: "#{status_code} error"
+  end
+
+  def require_action_enabled!
+    return unless DISABLE_EDITING
+
+    enabled_actions = ['show', 'index', 'hits_download']
+    unless enabled_actions.include?(params[:action])
+      raise Transition::FeatureUnavailableError.new(params[:controller], params[:action])
+    end
   end
 end

app/controllers/admin/import_urls_controller.rb
@@ -1,6 +1,5 @@
 require 'transition/import/urls'
 class Admin::ImportUrlsController < ApplicationController
-  include Transition::Controllers::ReadOnlyFilter
 
   def import
     @organisations = Organisation.order(:title)

app/controllers/hosts_controller.rb
@@ -1,5 +1,4 @@
 class HostsController < ApplicationController
-  include Transition::Controllers::ReadOnlyFilter
 
   # GET /hosts
   # GET /hosts.json

app/controllers/manual_urls_controller.rb
@@ -1,5 +1,4 @@
 class ManualUrlsController < ApplicationController
-  include Transition::Controllers::ReadOnlyFilter
 
   layout 'frontend'
 

app/controllers/mappings_controller.rb
@@ -1,5 +1,4 @@
 class MappingsController < ApplicationController
-  include Transition::Controllers::ReadOnlyFilter
 
   layout 'frontend'
 

app/controllers/organisations_controller.rb
@@ -1,5 +1,4 @@
 class OrganisationsController < ApplicationController
-  include Transition::Controllers::ReadOnlyFilter
 
   layout 'frontend'
   # GET /organisations

app/controllers/scrape_results_controller.rb
@@ -1,5 +1,4 @@
 class ScrapeResultsController < ApplicationController
-  include Transition::Controllers::ReadOnlyFilter
 
   layout 'frontend'
 

app/controllers/sites_controller.rb
@@ -1,5 +1,4 @@
 class SitesController < ApplicationController
-  include Transition::Controllers::ReadOnlyFilter
 
   layout 'frontend'
 

commit b844753364d58859ed145d718c7b0ffa3dc8f2ed

Don't apply edit access control to signin routes
The controllers related to SSO (signin and SSO Push API) were being blocked
because the action names were not whitelisted, but this filter shouldn't be
applied to them.

The bug was introduced in 8f4f86382d4b5d0ec926add4fe06deebc31d013b

app/controllers/application_controller.rb
@@ -29,6 +29,8 @@ class ApplicationController < ActionController::Base
 
   def require_action_enabled!
     return unless DISABLE_EDITING
+    gds_sso_controllers = [Api::UserController, AuthenticationsController]
+    return if gds_sso_controllers.detect { |skippable| self.is_a?(skippable) }
 
     enabled_actions = ['show', 'index', 'hits_download']
     unless enabled_actions.include?(params[:action])